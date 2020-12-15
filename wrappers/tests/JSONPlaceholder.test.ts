5634e290538a29f6b446d24a7ae94a9f62e1289c
Ian Scott <chesnekov@gmail.com>
2020-12-15 11:25:30 -0800

    update JsonPlaceholder.test.ts tests to use constants for HTTP response status codes to avoid magic numbers

--- wrappers/tests/JSONPlaceholder.test.ts
+++ wrappers/tests/JSONPlaceholder.test.ts
@@ -1,30 +1,39 @@
 import JSONPlaceholder from "../JSONPlaceholder/JSONPlaceholder";
 import { Post } from "../JSONPlaceholder/models/Post";
 
-const jsonPlaceholder = new JSONPlaceholder()
+describe('JsonPlaceholder', () => {
 
-test('get all Todos', async () => {
-  const res = await jsonPlaceholder.getAllTodos()
-  expect(res.status).toBe(200);
+  // Testing Constants
+  const jsonPlaceholder = new JSONPlaceholder();
+  const success = 200;
+  const notFound = 404;
+  const createdSuccessfully = 201;
+  //const 
   
+  test('get all Todos', async () => {
+    const res = await jsonPlaceholder.getAllTodos()
+    expect(res.status).toBe(success);
+    
+  });
+  
+  test('get todos ', async ()=>{
+    const res = await jsonPlaceholder.getTodoById(1)
+    const errorRes = await jsonPlaceholder.getTodoById(-100000)
+  
+    expect(res.status).toBe(success)
+    expect(errorRes.status).toBe(notFound)
+  
+  });
+  
+  test('post req post', async ()=>{
+    const post:Post = {
+      email:'test@test.com',
+      name: 'my name',
+      body: 'this is the body'
+    }
+    const res = await jsonPlaceholder.createPost(post)
+    expect(res.status).toBe(createdSuccessfully)
+    expect(res.data).toMatchObject(post)
+  });
+
 });
-
-test('get todos ', async ()=>{
-  const res = await jsonPlaceholder.getTodoById(1)
-  const errorRes = await jsonPlaceholder.getTodoById(-100000)
-
-  expect(res.status).toBe(200)
-  expect(errorRes.status).toBe(404)
-
-})
-
-test('post req post', async ()=>{
-  const post:Post = {
-    email:'test@test.com',
-    name: 'my name',
-    body: 'this is the body'
-  }
-  const res = await jsonPlaceholder.createPost(post)
-  expect(res.status).toBe(201)
-  expect(res.data).toMatchObject(post)
-})
