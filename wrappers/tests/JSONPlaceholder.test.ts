c5812a8fb551861c6a30b55b451fc1d642bcfa45
AlexGaiser <alexcgaiser@gmail.com>
2020-12-16 22:16:47 -0500

    updates tests to include create methods

--- wrappers/tests/JSONPlaceholder.test.ts
+++ wrappers/tests/JSONPlaceholder.test.ts
@@ -1,5 +1,8 @@
+import { post } from "../../services/requests";
 import JSONPlaceholder from "../JSONPlaceholder/JSONPlaceholder";
 import { Post } from "../JSONPlaceholder/models/Post";
+import { Todo } from "../JSONPlaceholder/models/Todo";
+import { User } from "../JSONPlaceholder/models/User";
 
 const jsonPlaceholder = new JSONPlaceholder()
 
@@ -24,7 +27,7 @@ test('post req post', async ()=>{
     name: 'my name',
     body: 'this is the body'
   }
-  const res = await jsonPlaceholder.createPost(post)
+  const res = await jsonPlaceholder.posts.create(post)
   expect(res.status).toBe(201)
   expect(res.data).toMatchObject(post)
 })
@@ -43,13 +46,48 @@ test('get todo with params', async()=>{``
   expect(response.data[0].id).toBe(1)
 })
 
+test('create todo', async ()=>{
+  const todo:Todo = {
+    userId: 1,
+    title: 'test todo title',
+    completed:false,
+  }
+  const response = await jsonPlaceholder.todos.create(todo);
+  expect(response.status).toBe(201);
+  expect(response.data).toMatchObject(todo);
+})
+
 test('users', async()=>{
+  const user:User = {
+    username:"test user",
+    name: "name namington",
+    email:"mail@mail.com",
+    address: {
+      street:"fake street",
+      city:"metropolis",
+      zipcode:"11111"
+    },
+    geo: {
+      lat: "111111",
+      lng: "1111111",
+    },
+    phone:"1234567890",
+    website: "www.web.com",
+    company: {
+      name: "Coolcompany, Inc",
+      catchPhrase: "The coolest company",
+      bs: "test bs"
+    }
+  }
   const userById = await jsonPlaceholder.getUserById(1)
   const allUsers = await jsonPlaceholder.getAllUsers()
   const findUsers = await jsonPlaceholder.findUsers({name: "Ervin Howell"})
+  const createdUser = await jsonPlaceholder.users.create(user)
   expect(userById.status).toBe(200)
   expect(userById.data.id).toBe(1)
   expect(allUsers.status).toBe(200)
   expect(allUsers.data.length).toBeGreaterThan(1)
   expect(findUsers.data[0].name).toBe("Ervin Howell")
+  expect(createdUser.data).toMatchObject(user)
+
 });
