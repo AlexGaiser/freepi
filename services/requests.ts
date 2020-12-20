f5ce1b2d9400deb821402e40a02846c631d99702
AlexGaiser <alexcgaiser@gmail.com>
2020-12-20 11:16:27 -0500

    Adds functional request builder

--- services/requests.ts
+++ services/requests.ts
@@ -1,6 +1,9 @@
 import Axios, { AxiosRequestConfig, AxiosPromise } from "axios"
+import { pipe } from "./utils.service";
 
 export const request = <T>(config:AxiosRequestConfig):AxiosPromise<T>=>{
+  config.url = encodeURI(config.url)
+
   return Axios(config).catch(e=>e.response);
 }
 
@@ -20,6 +23,51 @@ export const get = <T>(url:string, params?:AxiosRequestConfig):AxiosPromise<T> =
     ...params
   }
   return request<T>(config)
+}
+
+// for the request builder we will need the following:
+
+// set url
+// set method
+// set headers
+// set params
+// set data
+// extend url
+// extend params
+// extend data
+// extend headers
+// set property
+// extend property
+
+export const setReqProperty  =  (property:string)=> (value) => (request:AxiosRequestConfig):AxiosRequestConfig =>{
+  const newProperty = {property: value}
+  
+  return {
+    ...request,
+    ...newProperty
+  }
+}
+
+export const setReqUrl = (url:string)=> (request:AxiosRequestConfig):AxiosRequestConfig => {
+  const req: AxiosRequestConfig =
+  {
+    ...request,
+    url
+  }
+  return req
+}
+
+export const setReqMethod = (method:string) => (request):AxiosRequestConfig => {
+  const req: AxiosRequestConfig =
+  {
+    ...request,
+    method
+  }
+  return req
+}
+
+export const buildRequest = (...fns) => (request:AxiosRequestConfig):AxiosRequestConfig=> {
+  return pipe(...fns)(request)
 }
 
 const requests = {
