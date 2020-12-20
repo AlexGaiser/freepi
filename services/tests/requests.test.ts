58b77c65ffe9cc4174fd1197716d31a767b33024
AlexGaiser <alexcgaiser@gmail.com>
2020-12-20 10:46:24 -0500

    Adds test to request service

--- services/tests/requests.test.ts
+++ services/tests/requests.test.ts
@@ -1,6 +1,6 @@
 import { Post } from "../../wrappers/JSONPlaceholder/models/Post";
 import { Todo } from "../../wrappers/JSONPlaceholder/models/Todo";
-import requests from "../requests";
+import requests, { buildRequest, request, setReqMethod, setReqUrl } from "../requests";
 
 test('get request', async () => {
   const response = await requests.get<Todo>('https://jsonplaceholder.typicode.com/todos/1')
@@ -17,4 +17,15 @@ test('post request', async ()=>{
   const response = await requests.post<any>('https://jsonplaceholder.typicode.com/posts/', {data:{something:"something"}})
 
   expect(response.data).toStrictEqual({"id": 101,something:"something"})
+})
+
+test('test request builder', async ()=> {
+  const config = buildRequest(
+    setReqMethod('get'), 
+    setReqUrl('https://jsonplaceholder.typicode.com/posts')
+  )
+  ({})
+
+  const res = await request(config)
+  expect(res.status).toBe(200)
 })
