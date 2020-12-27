92cb7455158a4c924845af172f4c523fea057e00
Ian Scott <chesnekov@gmail.com>
2020-12-27 10:52:32 -0800

    issue #30 add createNested to base.req.ts.getBaseFunctions. Repositions createNested in the function hierarchy for organization within base.req.ts

--- wrappers/JSONPlaceholder/services/base.req.ts
+++ wrappers/JSONPlaceholder/services/base.req.ts
@@ -10,12 +10,6 @@ export const findNested = <T>(path:string) => (baseURL:string) => (nestedPaths:s
   const pathString = nestedPaths;
 }
 
-export const createNested = <T>(path:string) => (baseURL: string) => (nestedPaths:string[], params) => (data)  => {
-  const pathString = path + nestedPaths;
-  return post<T>('${baseURL}${pathString}', {data});
-}
-
-
 export const findAll =<T> (path:string) => (baseURL:string) =>  ()=>{
   return get<T[]>(`${baseURL}${path}/`)
 }
@@ -28,13 +22,18 @@ export const create = <T>(path:string)=> (baseURL:string)=> (data) => {
   return post<T>(`${baseURL}${path}`, {data})
 }
 
+export const createNested = <T>(path:string) => (baseURL: string) => (nestedPaths:string[], params) => (data)  => {
+  const pathString = path + nestedPaths;
+  return post<T>('${baseURL}${pathString}', {data});
+}
 
 
-export const getBaseFunctions= <T>(path:string) =>({url}:AxiosRequestConfig)=> {
+export const getBaseFunctions= <T>(path:string) =>({url}:AxiosRequestConfig) =>(nestedPaths:string[], params) => {
   return {
     find:find<T>(path)(url),
     findAll: findAll<T>(path)(url),
     getById: getById<T>(path)(url),
     create: create<T>(path)(url),
+    createNested: createNested<T>(path)(url)(nestedPaths, params),
   }
 }
