b09813703a2e9be0d8863118da7224095bdfd2d5
Ian Scott <chesnekov@gmail.com>
2021-02-02 09:15:54 -0800

    issue #30 corrects typo in albums constructor where incorrected nestedPath for photos was listed as albums

--- wrappers/JSONPlaceholder/services/albums.ts
+++ wrappers/JSONPlaceholder/services/albums.ts
@@ -2,7 +2,7 @@ import { Album } from '../models/Album';
 import { createNested, getBaseFunctions } from '../services/base.req';
 
 export const albums = (baseURL) => {
-  const nestedPaths: string[] = ["/albums"];
+  const nestedPaths: string[] = ["/photos"];
   return {
     ...getBaseFunctions<Album>('/Albums')(baseURL),
     createPhoto: createNested<Album>('/Albums')(baseURL.url)(nestedPaths[0]),
