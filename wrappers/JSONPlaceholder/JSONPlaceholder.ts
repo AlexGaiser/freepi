2b845813d21ce9b2529e0364a6d84d313df4b8c7
Ian Scott <chesnekov@gmail.com>
2021-02-02 09:39:59 -0800

    issue #20 cleans up legacy code in JSONPlaceholder.ts, removes unecessary import statements, empty constructor, and reference to services, which does not exist

--- wrappers/JSONPlaceholder/JSONPlaceholder.ts
+++ wrappers/JSONPlaceholder/JSONPlaceholder.ts
@@ -1,8 +1,3 @@
-import Axios, { AxiosResponse } from 'axios';
-import requests from '../../services/requests';
-import { Post } from './models/Post';
-import { Todo } from './models/Todo';
-import { User } from './models/User';
 import { todos } from './services/todos';
 import { users } from './services/users';
 import { posts } from './services/posts';
@@ -10,13 +5,8 @@ import { photos } from './services/photos';
 import { comments } from './services/comments';
 import { albums } from './services/albums';
 
-const { get, post } = requests;
 class JSONPlaceholder {
   private baseURL = 'https://jsonplaceholder.typicode.com';
-
-  constructor() {
-    // this.baseURL = 'https://jsonplaceholder.typicode.com'
-  }
 
   public todos = todos({ url: this.getBaseURL() });
   public users = users({ url: this.getBaseURL() });
@@ -27,70 +17,6 @@ class JSONPlaceholder {
 
   public getBaseURL(): string {
     return this.baseURL;
-  }
-
-  public findTodos(params) {
-    return get<Todo[]>(`${this.getBaseURL()}/todos`, { params });
-  }
-
-  public getTodoById(id: number) {
-    return get<Todo>(`${this.baseURL}/todos/${id}`);
-  }
-
-  public getAllTodos() {
-    return get<Todo[]>(`${this.baseURL}/todos/`);
-  }
-
-  public findPosts(params) {
-    return get<Post[]>(`${this.getBaseURL()}/posts/`, { params });
-  }
-
-  public getPostById(id: number) {
-    return get<Post>(`${this.baseURL}/posts/${id}`);
-  }
-
-  public getAllPosts() {
-    return get<Post[]>(`${this.baseURL}/posts/`);
-  }
-
-  public getCommentsByPost(id: number) {
-    return get<Post>(
-      `${this.getBaseURL()}/posts/${id}/comments`,
-    ).catch((e) => e.response);
-  }
-
-  public createPost(data: Post) {
-    return post<Post>(`${this.getBaseURL()}/posts/`, { data });
-  }
-
-  public updatePost(update) {
-    return Axios.put(`${this.getBaseURL()}/posts`, update).catch(
-      (e) => e.response,
-    );
-  }
-
-  public patchPost(update) {
-    return Axios.patch(`${this.getBaseURL()}/posts`, update).catch(
-      (e) => e,
-    );
-  }
-
-  public deletePost(id: number) {
-    return Axios.delete(`${this.getBaseURL()}/posts/${id}`).catch(
-      (e) => e,
-    );
-  }
-
-  public getAllUsers() {
-    return get<User[]>(`${this.getBaseURL()}/users/`);
-  }
-
-  public findUsers(params) {
-    return get<User[]>(`${this.getBaseURL()}/users/`, { params });
-  }
-
-  public getUserById(id: number) {
-    return get<User>(`${this.getBaseURL()}/users/${id}`);
   }
 }
 export default JSONPlaceholder;
