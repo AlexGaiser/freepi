0ab7ca200d57eb85ff70ff50fd9473596070e48e
AlexGaiser <alexcgaiser@gmail.com>
2021-02-10 13:04:20 -0500

    Issues #35 #36 makes more modifications to folder structure and renames services folder to core, sets up for publishing as separate package

---
+++ core/lib/index.ts
@@ -0,0 +1,13 @@
+import RequestBuilder from './req.builder';
+import requests from './requests';
+import utils from './utils.service';
+
+export { RequestBuilder, requests, utils };
+
+const core = {
+  RequestBuilder,
+  requests,
+  utils,
+};
+
+export default core;
---
+++ core/rollup.config.js
@@ -0,0 +1,39 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './lib/index.ts',
+  output: [
+    {
+      file: 'dist/cjs.js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.js',
+      format: 'es',
+      exports: 'named',
+    },
+    {
+      file: 'dist/umd.js',
+      format: 'umd',
+      name: 'umd',
+      exports: 'named',
+      globals: {
+        axios: 'axios',
+      },
+    },
+    {
+      file: 'dist/iife.js',
+      format: 'iife',
+      name: 'iife',
+      exports: 'named',
+      globals: {
+        axios: 'axios',
+      },
+    },
+  ],
+  plugins: [resolve(), commonjs(), typescript()],
+  external: ['axios'],
+};
