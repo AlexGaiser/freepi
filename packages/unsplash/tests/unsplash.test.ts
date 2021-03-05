da204d0a736970b4936114ac548520b5e8c77f6a
AlexGaiser <alexcgaiser@gmail.com>
2021-03-05 12:40:22 -0500

    Issue #64 updates tests unsplash class

--- packages/unsplash/tests/unsplash.test.ts
+++ packages/unsplash/tests/unsplash.test.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: `${__dirname}/../.env` });
 describe('Testing Basic Unsplash API Calls', () => {
   test('initializes unsplash', () => {
     console.log(process.env.UNSPLASH_CLIENT_ID);
-    const instance = Unsplash({
+    const instance = new Unsplash({
       access_key: process.env.UNSPLASH_CLIENT_ID,
     });
     expect(typeof instance.getAccessKey()).toBe('string');
