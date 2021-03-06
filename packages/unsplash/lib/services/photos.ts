6f5a50b15b0993a5695045988ef1c76db7e9b75a
AlexGaiser <alexcgaiser@gmail.com>
2021-03-06 00:49:52 -0500

    Issue #64, adds all methods for photos namespace, still needs testing

--- packages/unsplash/lib/services/photos.ts
+++ packages/unsplash/lib/services/photos.ts
@@ -5,11 +5,45 @@ interface RequestLib {
 }
 
 export const photos = (request: RequestLib) => {
+  const basePath = '/photos';
+  const get = (path, config = {}) => {
+    return request.get(`${basePath}${path}`, config);
+  };
   const getRandom = () => {
-    return request.get('/photos/random');
+    return get(`/random`);
+  };
+  const getById = (id: string) => {
+    return get(`/${id}`);
+  };
+
+  const getPhotoStatistics = (id: string) => {
+    return get(`/${id}/statistics`);
+  };
+  const trackDownload = (id: string) => {
+    return get(`/${id}/download`);
+  };
+
+  const updatePhoto = (id: string) => {
+    // waiting for put logic to be included
+    return;
+  };
+
+  const likePhoto = (id: string) => {
+    return get(`/${id}/like`);
+  };
+
+  const unLikePhoto = (id: string) => {
+    // delete logic needs to be added to core librarys
+    // return delete(`/${id}/like`);
   };
 
   return {
     getRandom,
+    getById,
+    getPhotoStatistics,
+    trackDownload,
+    updatePhoto,
+    likePhoto,
+    unLikePhoto,
   };
 };
