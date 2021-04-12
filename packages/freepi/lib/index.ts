adf9b8bab9d4aa49593c1216e8471ddab547560a
AlexGaiser <alexcgaiser@gmail.com>
2021-04-11 22:54:19 -0400

    Issue #92 Adds dino ipsum to main freepi package

--- packages/freepi/lib/index.ts
+++ packages/freepi/lib/index.ts
@@ -1,12 +1,14 @@
 import JSONPlaceholder from '@freepi/jsonplaceholder';
 import Unsplash from '@freepi/unsplash';
 import core from '@freepi/core';
+import DinoIpsum from '@freepi/dinoipsum';
 export { JSONPlaceholder, core, Unsplash };
 
 const freepi = {
   core,
   JSONPlaceholder,
   Unsplash,
+  DinoIpsum,
 };
 
 export default freepi;
