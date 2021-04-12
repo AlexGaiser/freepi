3aabe2f433a92f7068bb9cdf5218235b7545a5a3
AlexGaiser <alexcgaiser@gmail.com>
2021-04-12 09:33:42 -0400

    updates freepi alpha .2 adds dino ipsum to exported members

--- packages/freepi/lib/index.ts
+++ packages/freepi/lib/index.ts
@@ -2,7 +2,7 @@ import JSONPlaceholder from '@freepi/jsonplaceholder';
 import Unsplash from '@freepi/unsplash';
 import core from '@freepi/core';
 import DinoIpsum from '@freepi/dinoipsum';
-export { JSONPlaceholder, core, Unsplash };
+export { JSONPlaceholder, core, Unsplash, DinoIpsum };
 
 const freepi = {
   core,
